a0e531542c2b31b828cba3ad8f38650e19514f3d
Mevlüt Başaran <114837266+mevlt01001@users.noreply.github.com>
2024-07-13 23:31:40 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="feature_selection_toolkit",
-    version="0.2.3",
+    version="0.2.4",
     author="Mevlüt Başaran",
     author_email="mevlutbasaran01@gmail.com",
     description="A comprehensive toolkit for feature selection in machine learning.",
